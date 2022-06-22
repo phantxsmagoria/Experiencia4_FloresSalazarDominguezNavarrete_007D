f3680492e90bc483d50e6528729e9eecd5580522
phantxsmagoria <nxirmary@gmail.com>
2022-06-22 19:04:56 -0400

    BUG FIX Y CAMBIOS FORMULARIO

--- prueba3/urls.py
+++ prueba3/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, vision, mision, quienessomos, formulario, adoptados, galeria, registrousuario, mod_registrousuario,mostrardatos,del_registrousuario
+from .views import index, vision, mision, quienessomos, formulario, adoptados, galeria, registrousuario, mod_registrousuario, mostrardatos, del_registrousuario
 
 urlpatterns = [
     path('',index,name="index"),
@@ -11,6 +11,15 @@ urlpatterns = [
     path('adoptados.html',adoptados,name="adoptados"),
     path('galeria.html',galeria,name="galeria"),
     path('registrousuario.html',registrousuario,name="registrousuario"),
+    path('mod_registrousuario.html/index.html',index,name="index"),
+    path('mod_registrousuario.html/vision.html',vision,name="vision"),
+    path('mod_registrousuario.html/mision.html',mision,name="mision"),
+    path('mod_registrousuario.html/quienessomos.html',quienessomos,name="quienessomos"),
+    path('mod_registrousuario.html/formulario.html',formulario,name="formulario"),
+    path('mod_registrousuario.html/adoptados.html',adoptados,name="adoptados"),
+    path('mod_registrousuario.html/galeria.html',galeria,name="galeria"),
+    path('mod_registrousuario.html/registrousuario.html',registrousuario,name="registrousuario"),
+    path('mod_registrousuario.html/mostrardatos.html',mostrardatos,name="mostardatos"),
     path('mod_registrousuario.html/<id>',mod_registrousuario,name="mod_registrousuario"),
     path('del_registrousuario.html/<id>',del_registrousuario,name="del_registrousuario"),
     path('mostrardatos.html',mostrardatos,name="mostardatos"),
